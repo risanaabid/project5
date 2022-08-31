6fac5137d572c94012ae1380a6eab0656412293c
risanaabid <salmarisu756@gmail.com>
2022-08-31 09:47:29 +0530

    change code

--- project5/project5/settings.py
+++ project5/project5/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=r60rvd&#&h#iri=8$04w0kcb7=j5e--7$ay9wk=9!h6w_^5&d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
