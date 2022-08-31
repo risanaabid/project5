c169d36a1c38987f91220ac65fc904d5dd92863e
risanaabid <salmarisu756@gmail.com>
2022-08-31 10:54:41 +0530

    changecode

--- project5/user/url.py
+++ project5/user/url.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('master',views.user_master,name='master1'),
+    path('',views.user_master,name='master1'),
     path('home',views.user_home,name='home1'),
     path('about',views.user_about,name='about1'),
     path('contact',views.user_contact,name='contact1'),
